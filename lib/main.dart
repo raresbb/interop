7d44bfcfa4c260060c6ec7c4c6fa7b75442fd80e
ext-baroianu <rares-cristian.baroianu@stud.tu-darmstadt.de>
2023-01-23 23:58:47 +0100

    dart part not modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,70 @@ import 'dart:ffi';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:ffi/ffi.dart';
+import 'package:provider/provider.dart';
+
 
 class FFIBridge {
-  static bool initialize() {
-    nativeApiLib = Platform.isMacOS || Platform.isIOS
-        ? DynamicLibrary.process() // macos and ios
-        : (DynamicLibrary.open(Platform.isWindows // windows
-            ? 'api.dll'
-            : 'libapi.so')); // android and linux
+    final _startUpdates = DynamicLibrary.open(
+      Platform.isWindows ? 'api.dll' : 'libapi.so').
+        lookupFunction<Void Function(Pointer<NativeFunction<Void Function(Double, Double, Double, Double)>>), 
+      void Function(Pointer<NativeFunction<Void Function(Double, Double, Double, Double)>>)>('startUpdates');
 
-    final _getAngleFR = nativeApiLib.lookup<
-        NativeFunction<Double Function()>>('getAngleFR');
-    getAngleFR = _getAngleFR.asFunction<double Function()>();
+  final _stopAngles = DynamicLibrary.open(
+    Platform.isWindows ? 'api.dll' : 'libapi.so').
+      lookupFunction<Void Function(), void Function()>('stopUpdates');
 
-    final _getAngleFL = nativeApiLib.lookup<
-        NativeFunction<Double Function()>>('getAngleFL');
-    getAngleFL = _getAngleFL.asFunction<double Function()>();
-
-    final _getAngleRR = nativeApiLib.lookup<
-        NativeFunction<Double Function()>>('getAngleRR');
-    getAngleRR = _getAngleRR.asFunction<double Function()>();
-
-    final _getAngleRL = nativeApiLib.lookup<
-        NativeFunction<Double Function()>>('getAngleRL');
-    getAngleRL = _getAngleRL.asFunction<double Function()>();
-
-    return true;
+  void startUpdates(BuildContext context) {
+    final _callback = Pointer.fromFunction<Void Function(Double, Double, Double, Double)>(callback, context);
+    _startUpdates(_callback);
   }
 
-  static late DynamicLibrary nativeApiLib;
-  static late Function getAngleFR;
-  static late Function getAngleFL;
-  static late Function getAngleRR;
-  static late Function getAngleRL;
+  void stopUpdates() {
+    _stopAngles();
+  }
+  void callback(double angleFR, double angleFL, double angleRR, double angleRL, BuildContext context) {
+    final anglesModel = Provider.of<AnglesModel>(context, listen: false);
+    anglesModel.updateAngles([angleFR, angleFL, angleRR, angleRL]);
+  }
+  
+}
 
+
+class AnglesModel with ChangeNotifier {
+  double angleFR = 0.0;
+  double angleFL = 0.0;
+  double angleRR = 0.0;
+  double angleRL = 0.0;
+
+  void updateAngles(List<double> angles) {
+    angleFR = angles[0];
+    angleFL = angles[1];
+    angleRR = angles[2];
+    angleRL = angles[3];
+    notifyListeners();
+  }
 }
 
 void main() {
-  FFIBridge.initialize();
-  runApp(const MyApp());
+  final ffiBridge = FFIBridge();
+  ffiBridge.startUpdates(context);
+  runApp(
+    ChangeNotifierProvider(
+    create: (context) => AnglesModel(),
+    child: const MyApp(),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -61,20 +74,18 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
+    final anglesModel = Provider.of<AnglesModel>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -83,16 +94,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('AngleFR: ${FFIBridge.getAngleFR()}', style: const TextStyle(fontSize: 40)),
-            Text('AngleFL: ${FFIBridge.getAngleFL()}', style: const TextStyle(fontSize: 40)),
-            Text('AngleRR: ${FFIBridge.getAngleRR()}', style: const TextStyle(fontSize: 40)),
-            Text('AngleRL: ${FFIBridge.getAngleRL()}', style: const TextStyle(fontSize: 40)),
+            Text('AngleFR: ${anglesModel.angleFR}', style: const TextStyle(fontSize: 40)),
+            Text('AngleFL: ${anglesModel.angleFL}', style: const TextStyle(fontSize: 40)),
+            Text('AngleRR: ${anglesModel.angleRR}', style: const TextStyle(fontSize: 40)),
+            Text('AngleRL: ${anglesModel.angleRL}', style: const TextStyle(fontSize: 40)),
           ],
         ),
       ),
     );
   }
-} 
+}
+
 
 
 
--- libs/api.cpp
+++ libs/api.cpp
@@ -6,26 +6,43 @@
 
 #include <cstring>
 #include <ctype.h>
+#include <thread>
+#include <random>
+#include <functional>
+#include <atomic>
 
+std::atomic<bool> stop_thread{false};
+std::function<void(double, double, double, double)> callback;
+
+double angle_FR = 0.0;
+double angle_FL = 0.0;
+double angle_RR = 0.0;
+double angle_RL = 0.0;
+
+std::mt19937 generator(std::random_device{}());
+std::uniform_real_distribution<double> distribution(0.0, 90.0);
+
+void updateAnglesThread() {
+    while (!stop_thread) {
+        angle_FR = distribution(generator);
+        angle_FL = distribution(generator);
+        angle_RR = distribution(generator);
+        angle_RL = distribution(generator);
+        if (callback) {
+            callback(angle_FR, angle_FL, angle_RR, angle_RL);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
 
 EXPORT
-double getAngleFR() {
-    return 0.0;
-}
-
-EXPORT 
-double getAngleFL() {
-    return 1.0;
+void startUpdates(std::function<void(double, double, double, double)> cb) {
+    callback = cb;
+    std::thread t(updateAnglesThread);
+    t.detach();
 }
 
 EXPORT
-double getAngleRR() {
-    return 2.0;
+void stopUpdates() {
+    stop_thread = true;
 }
-
-EXPORT
-double getAngleRL() {
-    return 3.0;
-}
-
-
