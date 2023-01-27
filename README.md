cd737dbb775da89285354eddaea5bdb902d693e8
ext-baroianu <rares-cristian.baroianu@stud.tu-darmstadt.de>
2023-01-27 12:03:56 +0100

    implemented shared memory in C++ part

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## some commands for dart
+
+dart pub get      -> downloading dependencies
+flutter run       -> runs the app from terminal
+flutter run --hot -> enables hot reloading. This means that after the initial build, you can make code changes and see them reflected in the running app without having to rebuild the entire app.
--- libs/api.cpp
+++ libs/api.cpp
@@ -3,11 +3,12 @@
 #include <random>
 #include <atomic>
 #include <string>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <functional>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <cstring>
 
 std::mt19937 generator(std::random_device{}());
 std::atomic<bool> stop_thread{false};
@@ -21,7 +22,7 @@ struct Angles {
 
 // function that updates the angles struct
 void updateAngles(Angles* angles) {
-    std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+    std::uniform_real_distribution<double> distribution(-90.0, 90.0);
     auto random = std::bind(distribution, generator);
     while (!stop_thread) {
         angles->angle_FR = random();
@@ -29,35 +30,52 @@ void updateAngles(Angles* angles) {
         angles->angle_RR = random();
         angles->angle_RL = random();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // print all angles, one on each row
+        std::cout << "angle_FR: " << angles->angle_FR << std::endl;
+        std::cout << "angle_FL: " << angles->angle_FL << std::endl;
+        std::cout << "angle_RR: " << angles->angle_RR << std::endl;
+        std::cout << "angle_RL: " << angles->angle_RL << std::endl;
+        std::cout << "-----------------" << std::endl;
     }
 }
 
 int main() {
-    // create a named pipe
-    if (mkfifo("/tmp/AnglesPipe", 0666) == -1) {
-        std::cout << "Error creating named pipe" << std::endl;
-        return 1;
-    }
-
     Angles angles;
     // start the thread that updates the angles struct
     std::thread t(updateAngles, &angles);
 
-    int pipe;
-    // open the pipe for writing
-    pipe = open("/tmp/AnglesPipe", O_WRONLY);
-    if (pipe == -1) {
-        std::cout << "Error opening named pipe" << std::endl;
+    // create a shared memory object
+    int shm_fd = shm_open("/shm_angles", O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        std::cout << "Error creating shared memory object" << std::endl;
+        return 1;
+    }
+
+    // configure the size of the shared memory object
+    if (ftruncate(shm_fd, sizeof(angles)) == -1) {
+        std::cout << "Error configuring size of shared memory object" << std::endl;
+        return 1;
+    }
+
+    // memory map the shared memory object
+    void* ptr = mmap(0, sizeof(angles), PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (ptr == MAP_FAILED) {
+        std::cout << "Error memory mapping shared memory object" << std::endl;
         return 1;
     }
 
     while (!stop_thread) {
-        // write the angles struct to the pipe
-        write(pipe, &angles, sizeof(angles));
+        // write the angles struct to the shared memory object
+        memcpy(ptr, &angles, sizeof(angles));
     }
 
-    // close the pipe
-    close(pipe);
+    // unmap the shared memory object
+    if (munmap(ptr, sizeof(angles)) == -1) {
+        std::cout << "Error unmapping shared memory object" << std::endl;
+    }
+
+    // close the shared memory object
+    close(shm_fd);
 
     t.join();
     return 0;
