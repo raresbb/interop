1814d902a6ce069ea562639ec541fe505eb0726f
ext-baroianu <rares-cristian.baroianu@stud.tu-darmstadt.de>
2023-03-03 13:49:45 +0100

    script install flutter

--- angle_visualizer/install_flutter.sh
+++ angle_visualizer/install_flutter.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-sudo apt update
+# Install Dart
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install dart
+
+# Install Flutter
+sudo apt-get update
+sudo apt-get install git curl unzip xz-utils zip libglu1-mesa
+export PATH="$PATH:/usr/lib/dart/bin"
+git clone https://github.com/flutter/flutter.git
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter precache
 
 # Dart
 dart pub get
