c1388160efe2ff8d38076b70d5dd6b6f021667a9
raresbb <66315973+raresbb@users.noreply.github.com>
2023-03-03 14:29:54 +0100

    update install

--- angle_visualizer/install_flutter.sh
+++ angle_visualizer/install_flutter.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Install Dart
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
-sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
-sudo apt-get update
-sudo apt-get install dart
+# Get the current Dart SDK version
+current_version="$(dart --version | grep -oP 'version: \K\d+\.\d+\.\d+')"
+
+# Download the latest version of the Dart SDK
+latest_version="$(curl -s https://storage.googleapis.com/dart-archive/channels/stable/release/latest/VERSION | awk '{ print $2 }')"
+wget "https://storage.googleapis.com/dart-archive/channels/stable/release/$latest_version/sdk/dartsdk-linux-arm-release.zip"
+
+# Extract the downloaded archive to /opt/dart-sdk
+sudo mkdir /opt/dart-sdk
+sudo unzip dartsdk-linux-arm-release.zip -d /opt/dart-sdk
+sudo chown -R $USER:$USER /opt/dart-sdk
+rm dartsdk-linux-arm-release.zip
+
+# Update the system PATH to include the new Dart SDK location
+echo "export PATH=\"\$PATH:/opt/dart-sdk/bin\"" >> ~/.bashrc
+source ~/.bashrc
 
 # Install Flutter
 sudo apt-get update
 sudo apt-get install git curl unzip xz-utils zip libglu1-mesa
-export PATH="$PATH:/usr/lib/dart/bin"
 git clone https://github.com/flutter/flutter.git
 export PATH="$PATH:`pwd`/flutter/bin"
 flutter precache
 
-# Dart
+# Install dependencies for the Dart package
 dart pub get
