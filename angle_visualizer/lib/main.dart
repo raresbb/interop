16b07954c28f7fcc8e2f7aeb135c133f74b5f077
ext-baroianu <rares-cristian.baroianu@stud.tu-darmstadt.de>
2023-03-13 21:25:41 +0100

    dart working

--- angle_visualizer/lib/main.dart
+++ angle_visualizer/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:angle_visualizer/vehicle.dart';
 
@@ -10,7 +9,12 @@ class AngleData {
   double angleRR;
   double angleRL;
 
-  AngleData({required this.angleFR, required this.angleFL, required this.angleRR, required this.angleRL});
+  AngleData({
+    required this.angleFR, 
+    required this.angleFL, 
+    required this.angleRR, 
+    required this.angleRL
+    });
 }
 
 void main() {
@@ -53,32 +57,35 @@ class _RootPageState extends State<RootPage> {
     _readAngleData();
   }
 
-  // read angle data from pipe
+// read angle data from server socket
   Future<void> _readAngleData() async {
-  Timer.periodic(const Duration(seconds: 1), (_) async {
-    try {
-      var file = File('/tmp/AnglesPipe');
-      var randomAccessFile = await file.open(mode: FileMode.read);
-      var data = Uint8List(32);
+    // Bind to the specified IP address and port
+    var server = await ServerSocket.bind('127.0.0.1', 1234);
+    print('Listening on ${server.address.address}:${server.port}');
 
-      while (true) {
-        await randomAccessFile.readInto(data);
-        var buffer = data.buffer;
-        setState(() {
-          _angles = AngleData(
-            angleFR: buffer.asFloat64List()[0],
-            angleFL: buffer.asFloat64List()[1],
-            angleRR: buffer.asFloat64List()[2],
-            angleRL: buffer.asFloat64List()[3],
-          );
-        });
-      }
-    } catch (e) {
-      print(e);
-    }
-  });
-}
+    // Continuously listen for incoming connections
+    server.listen((socket) {
 
+      // Read incoming data from the socket
+      socket.listen((data) {
+        var values = String.fromCharCodes(data).split(',');
+          setState(() {
+            _angles = AngleData(
+              angleFR: double.parse(values[0]),
+              angleFL: double.parse(values[1]),
+              angleRR: double.parse(values[2]),
+              angleRL: double.parse(values[3]),
+            );
+          });
+      }, onError: (error) {
+        print('Error: $error');
+      }, onDone: () {
+      });
+    }, onError: (error) {
+      print('Error: $error');
+    });
+  }
+    
   @override
   Widget build(BuildContext context) {
     return Scaffold(
